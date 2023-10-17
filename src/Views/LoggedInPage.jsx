1d9337f934edeca3690fabc5da305a76645e83d2
Theo Esberg <esbergtheo@gmail.com>
2023-10-17 17:20:32 +0200

    Changed "LoggedInPage" to work with the API.

--- src/Views/LoggedInPage.jsx
+++ src/Views/LoggedInPage.jsx
@@ -1,14 +1,36 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
+const URLBase = "https://localhost:7154"
 
 const LoggedInPage = ({ email, password }) => {
-    console.log("Email in LoggedInPage:", email);
-    console.log("Password in LoggedInPage:", password);
+    const [data, setData] = useState([]);
+    const loginData = {
+        Email: email,
+        Password: password 
+    };
+
+    useEffect(() => {
+        // This will be our call to the API where we get the Employee back if the email and corresponding password is matching
+        fetch(URLBase + '/api/Login', {
+            method: 'POST',
+            headers: {
+                'Content-Type': 'application/json'
+            },
+            body: JSON.stringify(loginData)
+        })
+            .then((response) => response.json())
+            .then((data) => setData(data))
+            .catch((error) => console.error("Error: ", error));
+    }, [email, password]);
 
     return (
         <div>
             <h1>Welcome to the Logged In Page</h1>
-            <p>Email: {email}</p>
-            <p>Password: {password}</p>
+            <br /><hr /><br />
+            <p>ID: {data.id}</p>
+            <p>Name: {data.name}</p>
+            <p>Email: {data.email}</p>
+            <p>Vacation Days: {data.vacationDays}</p>
+            <p>Is Admin: {data.isAdmin ? 'Yes' : 'No'}</p>
         </div>
     );
 };
