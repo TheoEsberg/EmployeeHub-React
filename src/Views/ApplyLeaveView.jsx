7ff6516a6e1e768212391dbea961a0c98757fe41
Lykrat <emil.treptow@outlook.com>
2023-10-19 14:35:25 +0200

    Added a non functioning add LeaveRequest

--- src/Views/ApplyLeaveView.jsx
+++ src/Views/ApplyLeaveView.jsx
@@ -1,12 +1,37 @@
-import React from 'react';
+import React, { Fragment, useState } from 'react';
 import '../Css/ApplyLeave.css';
 
+import Container from 'react-bootstrap/Container';
+import Row from 'react-bootstrap/Row';
+import Col from 'react-bootstrap/Col';
+
 const ApplyLeaveView = () => {
+
+    const[LeaveTypeId, setLeaveTypeId]=useState("")
+    const[StartDate, setStartDate]=useState("")
+    const[EndDate, setEndDate]=useState("")
+
     return (
-        <div>
-            <h1>Apply Leave View</h1>
-            <p>This is the content of the Apply Leave page.</p>
-        </div>
+        <Fragment>
+            <h1>Apply for a Leave Request</h1>
+            <Container>
+                <Row>
+                    <Col>
+                        <input type="number" className="form-control" placeholder="Enter LeaveTypeID" value={LeaveTypeId} onChange={(x)=> setLeaveTypeId(x.target.value)}/>
+                    </Col>
+                    <Col>
+                        <input type="date" className="form-control" placeholder="Enter Start Date" value={StartDate} onChange={(x)=> setStartDate(x.target.value)}/>
+                    </Col>
+                    <Col>
+                        <input type="date" className="form-control" placeholder="Enter End Date" value={EndDate} onChange={(x)=> setEndDate(x.target.value)}/>
+                    </Col>
+                    <br></br>
+                    <Col>
+                        <button className='btn btn-primary' onClick={()=>handleAdd()}>Submit Leave Request</button>
+                    </Col>
+                </Row>
+            </Container>
+        </Fragment>
     );
 }
 
